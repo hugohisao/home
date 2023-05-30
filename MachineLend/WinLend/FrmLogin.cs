c8702d977c751fe8c83fde8057aeccb573c8c456
肖虎彪 Hubiao Xiao <CS07184@walsin.com>
2023-05-31 07:54:32 +0800

    20230531

--- MachineLend/WinLend/FrmLogin.cs
+++ MachineLend/WinLend/FrmLogin.cs
@@ -29,6 +29,23 @@ namespace WinLend
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
+            //獲取用戶輸入信息
+            string uName = txtUserName.Text.Trim();
+            string uPwd = txtUserPwd.Text.Trim();
+
+            //判斷是否為空
+            if (string.IsNullOrEmpty(uName))
+            {
+                MessageBox.Show("請輸入賬號！","登錄提示",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                txtUserName.Focus();
+                return;
+
+               // string i = "1";
+            }
+
+            //與數據庫通信 檢查輸入與數據庫是否一致
+
+            //返回的結果進行不同的提示
 
         }
     }
