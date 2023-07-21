6976873142dad63cbcdc3914ab2e18f8fe90fb6f
unknown <CS07184@walsin.com>
2023-07-21 11:58:00 +0800

    20230721

--- AndroidProjects/My Application/app/src/main/java/com/example/myapplication/MainActivity.java
+++ AndroidProjects/My Application/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    TextView tvContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.view2_layout);
+        tvContent = findViewById(R.id.tv_content);
+
+    }
+
+    public void changeText(View view) {
+        tvContent.setText("恭喜發財");
+        tvContent.setTextColor(Color.RED);
+        tvContent.setBackgroundColor(Color.YELLOW);
     }
 }
---
+++ AndroidProjects/My Application/app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -0,0 +1,57 @@
+package com.example.myapplication;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity3 extends AppCompatActivity {
+    TextView tvWalsin;
+    EditText etAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_layout);
+        //TextView textView = new TextView(this);
+        tvWalsin = findViewById(R.id.tv_walsin);
+        tvWalsin.setTypeface(Typeface.createFromAsset(getAssets(), "simkai.ttf"));
+        //tvWalsin.setText("華新");
+
+        //跑馬燈效果，獲取焦點
+        tvWalsin.setSelected(true);
+        //String text = (String) tvWalsin.getText();
+        String s = tvWalsin.getText().toString();
+        tvWalsin.setTextSize(50.0F);
+        tvWalsin.setTextColor(Color.YELLOW);
+
+        //設置EditText 輸入框光標的位置
+        etAccount = findViewById(R.id.et_account);
+        etAccount.setText("瘦唧唧的豬八戒");
+        String e = etAccount.getText().toString();
+        etAccount.setSelection(e.length() - 3);
+        //etAccount.requestFocus();
+
+        //設置ImageView背景圖片
+        ImageView ivPic = findViewById(R.id.iv_pic);
+
+//        ivPic.setImageResource(R.drawable.ic_launcher_background);
+
+//        getDrawable(R.drawable.walsin);
+        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.ic_launcher_background);
+        ivPic.setImageDrawable(drawable);
+        ivPic.setVisibility(View.INVISIBLE);
+
+    }
+
+    public void test() {
+
+    }
+}
